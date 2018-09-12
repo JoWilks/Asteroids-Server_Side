4a6f62903df985d5c3ac1b45287d15c805a66bec
Adham <admuaq@gmail.com>
2018-09-12 13:36:06 +0100

    Fixed top ten scores append on loading, added extra methods such as get top three scores by user id

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,5 +7,6 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-jo = User.create("Jo")
-score1 = Score.create(points: 1000, user_id: 1 )
+jo = User.create([{name: "Jo"},{name: "Adham"}, {name: "Bob"}, {name: "Jane"}, {name: "Jack"}, {name: "Jill"}])
+score1 = Score.create([{points: 4000, user_id: 1 },{points: 2000, user_id: 2 },{points: 150, user_id: 3 },
+    {points: 500, user_id: 4 },{points: 2500, user_id: 5 },{points: 200, user_id: 6 },{points: 10000, user_id: 1 },{points: 8000, user_id: 2 }])
